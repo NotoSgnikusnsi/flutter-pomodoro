50e52008c16cf126331b4bdb5f22ffe9c2b74bab
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-05-10 20:17:43 +0900

    feat: Refactor BreakingPageBody to use PomodoroTimer for timer functionality

--- lib/breaking_page.dart
+++ lib/breaking_page.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
+import './timer.dart';
+import 'dart:async';
 
 class BreakingPage extends StatelessWidget {
-  const BreakingPage({super.key, required data});
+  final int data;
+  const BreakingPage({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: const BreakingPageBody(),
+      body: BreakingPageBody(data: data),
     );
   }
 }
 
-class BreakingPageBody extends StatelessWidget {
-  const BreakingPageBody({super.key});
+class BreakingPageBody extends StatefulWidget {
+  final int data;
+  const BreakingPageBody({super.key, required this.data});
+
+  @override
+  State<BreakingPageBody> createState() => _BreakingPageBodyState();
+}
+
+class _BreakingPageBodyState extends State<BreakingPageBody> {
+  late int _initialMinutes;
+  late Timer? _timer;
+  bool _timerState = true;
+  late String _minutes = "";
+  late String _second = "";
+
+  final PomodoroTimer _pomodoroTimer = PomodoroTimer();
+
+  void _updateTime(Timer) {
+    String _currentMinutes =
+        _pomodoroTimer.getCurrentMinutes().toString().padLeft(2, "0");
+    String _currentSeconds =
+        _pomodoroTimer.getCurrentSeconds().toString().padLeft(2, "0");
+    if (_currentMinutes == "00" && _currentSeconds == "00") {
+      _pomodoroTimer.stopTimer();
+      _timer?.cancel();
+      Navigator.pop(context);
+    }
+
+    setState(() {
+      _minutes = _currentMinutes;
+      _second = _currentSeconds;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initialMinutes = widget.data;
+    _pomodoroTimer.startTimer(_initialMinutes, TimerState.breaking);
+    _timer = Timer.periodic(const Duration(seconds: 1), _updateTime);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +63,25 @@ class BreakingPageBody extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(40.0),
         child: SizedBox(
-          width: 400,
+          width: MediaQuery.of(context).size.width * 0.85,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                cursorColor: Theme.of(context).colorScheme.primary,
-                style: TextStyle(color: Theme.of(context).colorScheme.primary),
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.only(
-                      top: 0, bottom: 0, right: 10, left: 10),
-                  hintText: "Change what you do?",
-                  filled: true,
-                  fillColor: Theme.of(context).colorScheme.secondary,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+              Text(
+                "ポモドーロのアプリを作る",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 30,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.bold,
+                  height: 0,
                 ),
               ),
               const Padding(
                 padding: EdgeInsets.all(50.0),
               ),
               Text(
-                "Take A Break",
+                "breaking",
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.secondary,
                   fontSize: 24,
@@ -56,15 +94,15 @@ class BreakingPageBody extends StatelessWidget {
                 padding: EdgeInsets.all(10.0),
               ),
               Container(
-                width: 400,
-                height: 200,
+                width: MediaQuery.of(context).size.width * 0.85,
+                height: MediaQuery.of(context).size.height * 0.3,
                 decoration: BoxDecoration(
-                  color: Color(0xff518DF0),
+                  color: const Color(0xff518DF0),
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Center(
                   child: Text(
-                    "25:00",
+                    "$_minutes:$_second",
                     style: TextStyle(
                       color: Theme.of(context).colorScheme.secondary,
                       fontSize: 60,
@@ -76,9 +114,17 @@ class BreakingPageBody extends StatelessWidget {
                 ),
               ),
               IconButton(
-                onPressed: null,
+                onPressed: () {
+                  if (_timerState) {
+                    _pomodoroTimer.stopTimer();
+                    _timerState = false;
+                  } else {
+                    _pomodoroTimer.startTimer(0, TimerState.breaking);
+                    _timerState = true;
+                  }
+                },
                 icon: Icon(
-                  Icons.pause,
+                  _timerState ? Icons.pause : Icons.play_arrow,
                   color: Theme.of(context).colorScheme.secondary,
                   size: 60,
                 ),
