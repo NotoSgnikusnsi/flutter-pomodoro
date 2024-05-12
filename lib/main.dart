f12e088af39768ea524dece1ccc0f52485d480c5
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-05-12 19:46:48 +0900

    feat: Update app title to "PomoTimer"

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Pomodoro",
+      title: "PomoTimer",
       theme: ThemeData(
         colorScheme: const ColorScheme.light(
           primary: Color(0xff1e1e1e),
